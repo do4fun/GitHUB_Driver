d2a670dc1e1176a8d1c3db3272d821eae48b2cf6
Olena78 <Olena.Zinenko.1@collegeahuntsic.qc.ca>
2017-04-13 09:19:10 -0400

    Landscape
    Landscape

--- app/src/main/java/com/publiciteweb/mobile/github/Activity_driver.java
+++ app/src/main/java/com/publiciteweb/mobile/github/Activity_driver.java
@@ -1,17 +1,14 @@
 package com.publiciteweb.mobile.github;
 
 import android.content.Intent;
-import android.content.res.Configuration;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.ButtonBarLayout;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 
 import static android.graphics.Color.rgb;
-import static com.publiciteweb.mobile.github.R.drawable.button_press;
 
 /**
  * Created by sun on 17-04-10.
@@ -24,15 +21,77 @@ public class Activity_driver extends AppCompatActivity implements View.OnClickLi
     private Button btnConduite;
     private Button btnTravail;
     private Button btnFicheJournaliere;
+    private final int buttonColorBlue = rgb(0,102,153);
+    private static final String STATE_BUTTON = "buttonPressed";
+    private static final String STATE_REPOS = "buttonRepos";
+    private static final String STATE_COUCHETTE = "buttonCouchette";
+    private static final String STATE_CONDUIT = "buttonConduit";
+    private static final String STATE_TRAVAIL = "buttonTravail";
+    private static int NAME_BUTTON = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver);
-
         readContenApp();
+        if (savedInstanceState != null) {
+            // Restore value of members from saved state
+            /*btnRepos.setBackgroundColor(savedInstanceState.getInt(STATE_REPOS));
+            btnCouchette.setBackgroundColor(savedInstanceState.getInt(STATE_COUCHETTE));
+            btnConduite.setBackgroundColor(savedInstanceState.getInt(STATE_CONDUIT));
+            btnTravail.setBackgroundColor(savedInstanceState.getInt(STATE_TRAVAIL));*/
+            switch (NAME_BUTTON){
+                case R.id.btnRepos:
+                    btnRepos.setBackground(getDrawable(R.drawable.button_press));
+                    break;
+                case R.id.btnCouchette:
+                    btnCouchette.setBackground(getDrawable(R.drawable.button_press));
+                    break;
+                case R.id.btnConduite:
+                    btnConduite.setBackground(getDrawable(R.drawable.button_press));
+                    break;
+                case R.id.btnTravail:
+                    btnTravail.setBackground(getDrawable(R.drawable.button_press));
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt(STATE_BUTTON, NAME_BUTTON);
+        /*savedInstanceState.putInt(STATE_REPOS, ((ColorDrawable)btnRepos.getBackground()).getColor());
+        savedInstanceState.putInt(STATE_COUCHETTE, ((ColorDrawable)btnCouchette.getBackground()).getColor());
+        savedInstanceState.putInt(STATE_CONDUIT, ((ColorDrawable)btnConduite.getBackground()).getColor());
+        savedInstanceState.putInt(STATE_TRAVAIL, ((ColorDrawable)btnTravail.getBackground()).getColor());*/
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+       /* btnRepos.setBackgroundColor(savedInstanceState.getInt(STATE_REPOS));
+        btnCouchette.setBackgroundColor(savedInstanceState.getInt(STATE_COUCHETTE));
+        btnConduite.setBackgroundColor(savedInstanceState.getInt(STATE_CONDUIT));
+        btnTravail.setBackgroundColor(savedInstanceState.getInt(STATE_TRAVAIL));*/
+        switch (NAME_BUTTON){
+            case R.id.btnRepos:
+                btnRepos.setBackground(getDrawable(R.drawable.button_press));
+                break;
+            case R.id.btnCouchette:
+                btnCouchette.setBackground(getDrawable(R.drawable.button_press));
+                break;
+            case R.id.btnConduite:
+                btnConduite.setBackground(getDrawable(R.drawable.button_press));
+                break;
+            case R.id.btnTravail:
+                btnTravail.setBackground(getDrawable(R.drawable.button_press));
+                break;
+            default:
+                break;
+        }
+    }
     /*@Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
@@ -71,27 +130,31 @@ public class Activity_driver extends AppCompatActivity implements View.OnClickLi
         switch (v.getId()) {
             case R.id.btnRepos:
                 btnRepos.setBackground(getDrawable(R.drawable.button_press));
-                btnCouchette.setBackgroundColor(rgb(0,102,153));
-                btnConduite.setBackgroundColor(rgb(0,102,153));
-                btnTravail.setBackgroundColor(rgb(0,102,153));
+                NAME_BUTTON = R.id.btnRepos;
+                btnCouchette.setBackgroundColor(buttonColorBlue);
+                btnConduite.setBackgroundColor(buttonColorBlue);
+                btnTravail.setBackgroundColor(buttonColorBlue);
                 break;
             case R.id.btnCouchette:
                 btnCouchette.setBackground(getDrawable(R.drawable.button_press));
-                btnRepos.setBackgroundColor(rgb(0,102,153));
-                btnConduite.setBackgroundColor(rgb(0,102,153));
-                btnTravail.setBackgroundColor(rgb(0,102,153));
+                NAME_BUTTON = R.id.btnCouchette;
+                btnRepos.setBackgroundColor(buttonColorBlue);
+                btnConduite.setBackgroundColor(buttonColorBlue);
+                btnTravail.setBackgroundColor(buttonColorBlue);
                 break;
             case R.id.btnConduite:
                 btnConduite.setBackground(getDrawable(R.drawable.button_press));
-                btnRepos.setBackgroundColor(rgb(0,102,153));
-                btnCouchette.setBackgroundColor(rgb(0,102,153));
-                btnTravail.setBackgroundColor(rgb(0,102,153));
+                NAME_BUTTON = R.id.btnConduite;
+                btnRepos.setBackgroundColor(buttonColorBlue);
+                btnCouchette.setBackgroundColor(buttonColorBlue);
+                btnTravail.setBackgroundColor(buttonColorBlue);
                 break;
             case R.id.btnTravail:
                 btnTravail.setBackground(getDrawable(R.drawable.button_press));
-                btnRepos.setBackgroundColor(rgb(0,102,153));
-                btnCouchette.setBackgroundColor(rgb(0,102,153));
-                btnConduite.setBackgroundColor(rgb(0,102,153));
+                NAME_BUTTON = R.id.btnTravail;
+                btnRepos.setBackgroundColor(buttonColorBlue);
+                btnCouchette.setBackgroundColor(buttonColorBlue);
+                btnConduite.setBackgroundColor(buttonColorBlue);
                 break;
             case R.id.btnFicheJournaliere:
                 Intent intentFicheJournaliere = new Intent(Activity_driver.this, Activity_ficheJournaliere.class);
